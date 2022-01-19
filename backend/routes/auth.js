b58bcf74d4c1e3c85228a1786e800b0a9a6d7a17
Prasoon Soni <prasoonsoni03@gmail.com>
2022-01-19 14:59:08 +0530

    Creating collections in Postman to manage requests and adding validations.

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const User = require("../models/User");
 const { body, validationResult } = require("express-validator");
 
-// create a user using: POST "/api/auth/". doesnt require auth
+// create a user using: POST "/api/auth/createuser". No Login Required
 router.post(
-    "/",
+    "/createuser",
     [
         body("name", "Enter a valid name").isLength({ min: 3 }),
         body("email", "Enter a valid email").isEmail(),
@@ -13,21 +13,29 @@ router.post(
             min: 5,
         }),
     ],
-    (req, res) => {
+    async (req, res) => {
+        // if there are errors, return bad request and the errors
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             return res.status(400).json({ errors: errors.array() });
         }
-        User.create({
-            name: req.body.name,
-            email: req.body.email,
-            password: req.body.password
-        })
-        .then(user => res.json(user))
-        .catch(err=>{
-            console.log(err);
-            res.json({error:"Please enter a unique value for email", message:err.message});
-        });
+        // check whether the user with same email exists already
+        try {
+            let user = await User.findOne({ email: req.body.email });
+            if (user) {
+                return res.status(400).json({ error: "User already exists" })
+            }
+            user = await User.create({
+                name: req.body.name,
+                email: req.body.email,
+                password: req.body.password
+            });
+            res.json(user);
+        } catch (error) {
+            console.error(error.message);
+            res.status(500).send("Some error occured");
+        }
+
     }
 );
 
