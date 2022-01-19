46497fc334f040cff7d55d2eed559a388e29cd3b
Prasoon Soni <prasoonsoni03@gmail.com>
2022-01-19 13:14:15 +0530

    Creating routes & Schema for storing notes & users

--- backend/index.js
+++ backend/index.js
@@ -4,9 +4,9 @@ connectToMongo();
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello Prasoon!')
-})
+// Available Routes
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
