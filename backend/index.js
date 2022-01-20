3b887df926b65198183f8d8ebe939e625492fab1
Prasoon Soni <prasoonsoni03@gmail.com>
2022-01-20 16:07:07 +0530

    Created a middleware to decode user from JWT

--- backend/index.js
+++ backend/index.js
@@ -10,9 +10,10 @@ const port = 5000
 app.use(express.json());
 
 // Available Routes
-app.use('/api/auth/createuser', require('./routes/auth/createuser'))
-app.use('/api/auth/login', require('./routes/auth/login'))
-app.use('/api/notes', require('./routes/notes'))
+app.use('/api/auth/createuser', require('./routes/auth/createuser'));
+app.use('/api/auth/login', require('./routes/auth/login'));
+app.use('/api/auth/getuser', require('./routes/auth/getuser'));
+app.use('/api/notes', require('./routes/notes'));
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
---
+++ backend/routes/auth/getuser.js
@@ -0,0 +1,24 @@
+require('dotenv').config()
+const express = require("express");
+const router = express.Router();
+const User = require("../../models/User");
+const { body, validationResult } = require("express-validator");
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const fetchuser = require('../../middleware/fetchuser');
+
+router.post('/',fetchuser, async (req, res) => {
+    try {
+        const userID = req.user.id;
+        const user = await User.findById(userID).select('-password');
+        res.send(user);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Some internal server occured.");
+    }
+
+});
+
+
+module.exports = router;
+
