bb58b55ed5188c8e4693ce97d1458f4fa78a0388
Prasoon Soni <prasoonsoni03@gmail.com>
2022-01-20 12:51:31 +0530

    Hashing Passwords using bcryptjs

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 const { body, validationResult } = require("express-validator");
-
+const bcrypt = require('bcryptjs');
 // create a user using: POST "/api/auth/createuser". No Login Required
 router.post(
     "/createuser",
@@ -12,8 +12,7 @@ router.post(
         body("password", "Password must be atleast 5 characters").isLength({
             min: 5,
         }),
-    ],
-    async (req, res) => {
+    ], async (req, res) => {
         // if there are errors, return bad request and the errors
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
@@ -21,14 +20,21 @@ router.post(
         }
         // check whether the user with same email exists already
         try {
+            // checking if user already exists or not
             let user = await User.findOne({ email: req.body.email });
+            // if user already exists then sending response
             if (user) {
                 return res.status(400).json({ error: "User already exists" })
             }
+            // hashing the password
+            const salt = await bcrypt.genSalt(10);
+            const secPass = await bcrypt.hash(req.body.password, salt);
+            
+            // creating user
             user = await User.create({
                 name: req.body.name,
                 email: req.body.email,
-                password: req.body.password
+                password: secPass
             });
             res.json(user);
         } catch (error) {
