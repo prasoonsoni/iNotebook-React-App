51bc5f08221d61a9f2bcee358aee5e5a1d788e71
Prasoon Soni <prasoonsoni03@gmail.com>
2022-01-21 21:53:58 +0530

    Added endpoint for updating a note.

---
+++ backend/routes/notes/updatenote.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const router = express.Router();
+const Note = require("../../models/Note");
+const { body, validationResult } = require("express-validator");
+const fetchuser = require("../../middleware/fetchuser");
+
+router.put('/:id', fetchuser, async(req, res)=>{
+    const{title, description, tag} = req.body;
+
+    // create newNote Object
+    const newNote = {};
+    if(title){
+        newNote.title = title;
+    }
+    if(description){
+        newNote.description = description;
+    }
+    if(tag){
+        newNote.tag = tag;
+    }
+
+    // find the note to be updates
+    let note = await Note.findById(req.params.id);
+    if(!note){res.status(404).send("Not Found")};
+
+    if(note.user.toString() !== req.user.id){
+        return res.status(401).send("Not Allowed");
+    }
+    note = await Note.findByIdAndUpdate(req.params.id, {$set:newNote}, {new:true});
+    res.json({note});
+});
+
+module.exports = router;
